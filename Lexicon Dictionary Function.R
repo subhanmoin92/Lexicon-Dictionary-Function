acae321b2b9d20541724c9eed18357609e37ae81
Syed Abdul Subhan Moin <33136746+subhanmoin92@users.noreply.github.com>
2017-11-29 11:36:23 -0600

    Update Lexicon Dictionary Function.R

--- Lexicon Dictionary Function.R
+++ Lexicon Dictionary Function.R
@@ -1,18 +1,18 @@
-install.packages("rvest")
+install.packages("rvest")  //installing the package rvest for web scraping
 install.packages("dplyr")
 library(dplyr)
 library(rvest)
 
 ##creating a lexicon file and saving it 
 
-url<-"https://www.powerthesaurus.org/surprise/synonyms/"
+url<-"https://www.powerthesaurus.org/surprise/synonyms/"        //getting the words from a dictionary page 
 A <- NULL
-N_pages<-52
+N_pages<-52                                                     //considering the no of web pages to be 52
 D<-NULL
 for (j in 2: N_pages){
   pixel <- read_html(paste0(url, j)) 
-  B <- cbind(pixel %>% html_nodes(".topic-link span , .topic-link") %>%     html_text()     )
-  A <- rbind(A,B)
+  B <- cbind(pixel %>% html_nodes(".topic-link span , .topic-link") %>%     html_text()     )       //using selector gadget to get words
+  A <- rbind(A,B)                                                                                   //binding into a final matrix
 }
 
-write(A, file="surprise_words.txt")
+write(A, file="surprise_words.txt")                              //saving the file into local system
